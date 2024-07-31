9a7536c415408ec0bc3b879dadc1929d974a21ba
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2024-07-31 11:57:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ wss.on('connection', (ws) => {
 });
 
 // Start the HTTP server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
     console.log(`Server is listening on https://stutass.onrender.com`);
